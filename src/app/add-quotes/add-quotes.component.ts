6f0a6b8f8824ac96c5b7dcaa05231507dfcb65d9
Bakytai Egemberdieva <bakijw@gmail.com>
2021-12-11 16:38:31 +0600

    прописала логику на добавление цитат

--- src/app/add-quotes/add-quotes.component.ts
+++ src/app/add-quotes/add-quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-quotes',
@@ -6,16 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-quotes.component.css']
 })
 export class AddQuotesComponent implements OnInit {
-  category!: string;
-  text!: string;
-  author!: string;
+  categoryName!: string;
+  userText!: string;
+  authorName!: string;
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   createQuote() {
+    const category = this.categoryName;
+    const author = this.authorName;
+    const text = this.userText;
+    const body = {category, author, text};
 
+    this.http.post('https://plovo-13-default-rtdb.firebaseio.com/quotes.json', body).subscribe();
   }
 }
